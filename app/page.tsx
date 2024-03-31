a82290f65787e50ec53361d6cec2e9770ba00eec
Hamza Outmassint <hamzaayrom@gmail.com>
2024-03-31 00:54:22 +0000

    navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-black h-screen">
       <h1>hello world</h1>
+      
+      <Button className="bg-primary text-white hover:bg-white hover:text-primary">companies</Button>
     </main>
   );
 }
--- components/navabar.tsx
+++ components/navabar.tsx
@@ -1,19 +1,37 @@
 import Image from "next/image"
-import commonShareLogo from '../public/commonShare-logo.svg'
+import commonShareLogo from '../public/assest/icons/commonShare-logo.svg'
+import searchIcon from '../public/assest/icons/search.svg'
 import Link from "next/link"
-import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue, } from "@/components/ui/select"
 
 const Navabar = () => {
   return (
-    <nav className='flex justify-between items-center p-4 border-b border-gray-light c'>
-      <div className="flex justify-center items-center">
+    <nav className='grid grid-cols-5 place-items-center place-content-start py-4 pl-4 border-b border-gray-light fixed z-50 bg-white w-full'>
+      <Link href='/'  className="place-self-start">
         <Image src={commonShareLogo} alt="common share logo" height={30} />
-        <div className="text-sm">
-          <Button className="bg-primary text-white hover:bg-white hover:text-primary">companies</Button>
+      </Link>
+      <form className="text-sm col-span-3 place-self-start flex justify-center items-center border border-gray-light rounded-2xl">
+        <Select>
+          <SelectTrigger className="w-[120px]">
+            <SelectValue placeholder="Companies" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Sumsang</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <div className="flex relative items-center ml-[2px]">
+          <Input type="text" placeholder="Search Sustainable Companies" className="w-[35rem] border-y border-r border-l-none border-gray-light px-4 py-2 focus-visible:ring-offset-0 rounded-l-none rounded-r-2xl" />
+          <Image src={searchIcon} alt="search icon" height={17} className="absolute right-4" />
         </div>
-      </div>
+      </form>
       <ul className="flex justify-between text-base font-bold cursor-pointer">
-        <li className="relative hover:text-primary transition ease-linear duration-150 px-3" id="login">Login</li>
+        <li className="relative hover:text-primary transition ease-linear duration-150 px-3" id="login">
+          Login
+        </li>
         <li className="relative hover:text-primary transition ease-linear duration-150 px-3 mx-1" id="sign-up">
           <Link href='https://www.commonshare.com/register'> Sign up </Link>
         </li>
