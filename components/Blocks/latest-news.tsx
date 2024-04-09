77b8ff305856f4aab9654651f45744f523866e75
Hamza Outmassint <hamzaayrom@gmail.com>
2024-04-09 00:37:23 +0000

    i add few changes in latest news section

--- components/Blocks/latest-news.tsx
+++ components/Blocks/latest-news.tsx
@@ -8,7 +8,11 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
+import { ChevronRight } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
+
 import Blogs from '../blogs-data';
+import Logo from '../../public/assest/logo/Images.svg';
 
 const LatestNews = () => {
     return (
@@ -23,18 +27,28 @@ const LatestNews = () => {
             </div>
             <div className='h-[40rem]'>
                 <Swiper
-                    navigation={{ nextEl: '.out', prevEl: '.in' }}
+                    navigation={{ nextEl: '.next', prevEl: '.prev' }}
                     modules={[Navigation]}
-                    slidesPerView={3} spaceBetween={15} loop={true}
+                    slidesPerView={3} spaceBetween={20} loop={true}
                     className="mySwiper">
                     {
                         Blogs.map((ele, index) => {
                             return (
-                                <SwiperSlide key={index}>
-                                    <div className='relative'>
-                                        <span className='bg-white rounded-full px-4 py-2 absolute top-4 left-4 text-xs font-semibold hidden group-hover:flex'>Sustainable Supply Chains</span>
-                                        <div className='h-[202px] flex justify-center items-center overflow-hidden group '>
-                                            <Image src={ele['img-path']} alt={ele['title']} />
+                                <SwiperSlide key={index} >
+                                    <div className='flex flex-col mt-16'>
+                                        <div className='relative h-[202px] flex justify-center items-center overflow-hidden group rounded-md transition duration-150 ease-linear'>
+                                            <span className='bg-white rounded-full px-4 py-2 absolute top-4 left-4 text-xs font-semibold hidden group-hover:flex'>
+                                                Sustainable Supply Chains
+                                            </span>
+                                            <Image src={ele['img-path']} alt={ele['title']} height={202} />
+                                            <span className='flex items-center justify-center absolute bottom-4 left-4'>
+                                                <Image src={Logo} alt='commonshare logo' height={30}/>
+                                                <span className='ml-3 text-white text-base font-medium'>Community Updates</span>
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <h1 className='font-semibold text-2xl line-clamp-3 mt-2'>{ele.title}</h1>
+                                            <p className='line-clamp-2 text-gray-bold mt-4'>{ele.body}</p>
                                         </div>
 
                                     </div>
@@ -44,9 +58,10 @@ const LatestNews = () => {
                     }
 
                 </Swiper>
-                <div>
-                    <Button className='out'>out</Button>
-                    <Button className='in'>in</Button>
+                <div className='flex items-center justify-center mt-10'>
+                    <Button className='next mr-3 bg-white hover:bg-primary text-primary hover:text-white border border-primary'><ChevronLeft size={20}/></Button>
+                    <Button className='bg-white hover:bg-primary text-primary hover:text-white border border-primary'>All Updates</Button>
+                    <Button className='prev ml-3 bg-white hover:bg-primary text-primary hover:text-white border border-primary'><ChevronRight size={20}/></Button>
                 </div>
             </div>
         </div>
