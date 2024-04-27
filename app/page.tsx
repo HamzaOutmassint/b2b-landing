b897495a6a96d05c4869f446675ed3f2aa32f6f0
Hamza Outmassint <hamzaayrom@gmail.com>
2024-04-27 20:02:39 +0100

    i renamed the components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import BrandShowcase from "../components/blocks/brand-showcase";
-import ExploreSection from "../components/blocks/explore-section";
-import HeroSection from "../components/blocks/hero-section";
-import InformativeContent from "../components/blocks/informative-content";
-import LatestNews from "../components/blocks/latest-news";
-import SpecificBrands from "../components/blocks/specific-brands";
-import SustainablePartnerships from "../components/blocks/sustainable-partnerships";
-import UkSustainableBrands from "../components/blocks/uk-sustainable-brands";
+import BrandShowcase from "@/components/blocks/brand_showcase";
+import ExploreSection from "@/components/blocks/explore_section";
+import HeroSection from "@/components/blocks/hero_section";
+import InformativeContent from "@/components/blocks/informative_content";
+import LatestNews from "@/components/blocks/latest_news";
+import SpecificBrands from "@/components/blocks/specific_brands";
+import SustainablePartnerships from "@/components/blocks/sustainable_partnerships";
+import UkSustainableBrands from "@/components/blocks/uk_sustainable_brands";
 
 export default function Home() {
   return (
